2109e5205d5631a0258b94d374244123777063a9
Julian Werner <info@werner-julian.de>
2023-04-24 20:22:28 +0200

    feat: add displays

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <TM1637Display.h>
 
-SoftwareSerial mySerial(18, 19); // RX, TX
+SoftwareSerial mySerial(15, 17); // RX, TX
+TM1637Display display0(14, 8);
+TM1637Display display1(14, 9);
+TM1637Display display2(14, 10);
+TM1637Display display3(14, 11);
+TM1637Display display4(14, 12);
 long intervall = 0;
 int tries = 0;
+int seconds = 0;
+int minutes = -1;
+int hours = 0;
+unsigned long milliStart = 0;
+boolean pointActive = false;
 boolean activ = false;
 boolean incommingMessage = false;
 
@@ -11,6 +22,7 @@ boolean incommingMessage = false;
 #define RESET 16
 #define LED 13
 #define FAIL_LED 17
+#define BRIGTHNESS 12
 
 // XX = country code, e.g. "49" for Germany and xxxxxxxxxxx = phone number
 const char TELEFONE_NUMBER[] = "+491729999128";
@@ -21,14 +33,38 @@ void errorHandling();
 void resetSim();
 String getSMS();
 void ring();
+void handleRelais(String);
+
+int delayVal = 50;
+int pause = 12;
 
 void setup()
 {
+  // Setzt die Helligkeit des Displays mögliche Werte sind 0 bis 15
+  display0.setBrightness(BRIGTHNESS);
+  display1.setBrightness(BRIGTHNESS);
+  display2.setBrightness(BRIGTHNESS);
+  display3.setBrightness(BRIGTHNESS);
+  display4.setBrightness(BRIGTHNESS);
+  uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
+  // Setzt die Anzahl der möglichen Segmente.
+  display0.setSegments(data);
+  display1.setSegments(data);
+  display2.setSegments(data);
+  display3.setSegments(data);
+  display4.setSegments(data);
+
   pinMode(RESET, OUTPUT);
   digitalWrite(RESET, HIGH);
   pinMode(LED, OUTPUT);
   pinMode(FAIL_LED, OUTPUT);
   digitalWrite(FAIL_LED, HIGH);
+  for (int i = 0; i <= 10; i++)
+  {
+    pinMode(i + 1, OUTPUT);
+    digitalWrite(i + 1, LOW);
+  }
+
   Serial.begin(9600);
   mySerial.begin(9600);
 
@@ -37,7 +73,7 @@ void setup()
   TCNT2 = 0;   // Timer Counter 2
   TCCR2A = 0;  // Loesche Timer Counter Controll Register A
   TCCR2B = 0;  // Loesche Timer Counter Controll Register B
-  OCR2A = 155; // Setze Output Compare Register A
+  OCR2A = 312; // Setze Output Compare Register A
   // Setze CS20, CS21 und CS22 - Clock Select Bit 10,11,12 (Prescaler 1024)
   TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
   // CTC-Mode ein
@@ -45,6 +81,7 @@ void setup()
   // Timer/Counter Interrupt Mask Register
   TIMSK2 |= (1 << OCIE2A); // Output Compare A Match Interrupt Enable
 
+  milliStart = millis();
   sei(); // allow interrupts
 
   Serial.println("Initializing...");
@@ -64,28 +101,35 @@ void loop()
     Serial.println("Data available");
     String message = mySerial.readString();
     Serial.println(message);
-    message.toLowerCase();
-    int index = message.indexOf("on");
-    boolean on = index >= 0;
-    String sendMessage = "";
-    if (message.indexOf("relais1") > -1)
-    {
-      sendMessage = "Relais 1 is ";
-      sendMessage += on ? "on" : "off";
-      Serial.println(sendMessage);
-      sendSMS(sendMessage);
-    }
-    else if (message.indexOf("relais2") > -1)
-    {
-      sendMessage = "Relais 2 is ";
-      sendMessage += on ? "on" : "off";
-      Serial.println(sendMessage);
-      digitalWrite(FAIL_LED, on);
-      sendSMS(sendMessage);
-    }
+    handleRelais(message);
     message = "";
   }
   delay(500);
+}
+
+void handleRelais(String message)
+{
+  message.toLowerCase();
+  String sendMessage = "";
+  if (message.indexOf("relais") > -1)
+  {
+    int index = message.indexOf("relais");
+    int relais = message.substring(index + 6, index + 8).toInt();
+    if (relais > 0 && relais < 6)
+    {
+      boolean on = message.indexOf("on") > -1;
+      sendMessage = "Relais " + String(relais) + " is ";
+      sendMessage += on ? "on" : "off";
+      Serial.println(sendMessage);
+      digitalWrite(1 + relais, on);
+      sendSMS(sendMessage);
+    }
+    else
+    {
+      Serial.println("Relais " + String(relais) + " not found");
+      sendSMS("Relais " + String(relais) + " not found");
+    }
+  }
 }
 
 String updateSerial(String message = "")
@@ -119,6 +163,7 @@ String updateSerial(String message = "")
 
 void errorHandling()
 {
+  digitalWrite(FAIL_LED, HIGH);
   activ = false;
   Serial.println("Start Checking");
   int fail = -1;
@@ -167,6 +212,7 @@ void errorHandling()
 
   Serial.println("Sim Registered");
   activ = true;
+  digitalWrite(FAIL_LED, LOW);
 }
 
 void resetSim()
@@ -188,6 +234,7 @@ void sendSMS(String message)
 
   // SMS text content
   String tmp = updateSerial(message);
+  handleRelais(tmp);
   Serial.println(tmp);
   mySerial.write(26);
   String back = updateSerial();
@@ -208,7 +255,6 @@ void sendSMS(String message)
   }
   tries = 0;
   Serial.println("Successfully sended");
-  digitalWrite(5, LOW);
 }
 
 String getSMS()
@@ -229,8 +275,32 @@ String getSMS()
 
 ISR(TIMER2_COMPA_vect)
 {
-  if (!(intervall++ % 100))
+  if (millis() - milliStart > 1000)
   {
-    digitalWrite(LED, !digitalRead(LED));
+    milliStart += 1000;
+    if (seconds == 0)
+    {
+      minutes++;
+    }
+    display0.showNumberDecEx(seconds++, 0b01000000, true, 4, 0);
+    if (seconds > 59)
+    {
+      seconds = 0;
+    }
+
+    if (minutes > 59)
+    {
+      minutes = 0;
+      hours++;
+    }
+    pointActive = !pointActive;
+    display1.showNumberDecEx(minutes + hours * 100, pointActive ? 0b01000000 : 0, true, 4, 0);
+    display2.showNumberDecEx(minutes + hours * 100, pointActive ? 0b01000000 : 0, true, 4, 0);
+    display3.showNumberDecEx(minutes + hours * 100, pointActive ? 0b01000000 : 0, true, 4, 0);
+    display4.showNumberDecEx(minutes + hours * 100, pointActive ? 0b01000000 : 0, true, 4, 0);
   }
+  if(millis() < milliStart) {
+    milliStart = millis();
+  }
+  digitalWrite(LED, !digitalRead(LED));
 }
